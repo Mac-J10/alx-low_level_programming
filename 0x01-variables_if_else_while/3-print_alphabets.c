cc7ec4b57007a4da779e4f67ee338669c1414a4d
Mac-J10 <jacksonjay916@gmail.com>
2022-09-18 12:39:04 +0300

    3-print

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio>[D.h[>
+/**
+ *main - print if the number is positive, zero or negative
+ *
+ *Description: using the main fnction[D[D[D[D[D[Dunction
+ *this program prints "Programming is positive, zero or negtive
+ Return: 0
+[A[C*Return: 0
+ */
+
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++);
+{
+	putchar(ch);
+}
+for (ch = 'A'; ch < 'Z'; ch++);[D[D[D[D[D[D[D[D[D[D[D[D= 'Z'; ch++);
+{
+	putchar(ch);
+}
+putchar('\n');
+return (0);
+}
