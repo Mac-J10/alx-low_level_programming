67c9502c1c5038f2860ab40d711d04651ee4a09b
Mac-J10 <jacksonjay916@gmail.com>
2022-09-18 12:23:58 +0300

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - print if the number i positive, zero or negative
+*
+*Description: using the main function
+*this program prints "Programming is positive, zero or negative
+*Return: 0
+*/
+int main()[Dvoid)
+{
+	char c;
+	for (ch = 'a' ; ch <= 'z' ; ch ++);
+[D[D[D[D[D[D{
+		putchar(ch);
+	}
+	putchar('\n');
+return (0);
+}
