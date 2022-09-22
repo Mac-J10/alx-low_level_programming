a5cea64617c8505cff76984ef3c4be2612b4f40e
Mac-J10 <jacksonjay916@gmail.com>
2022-09-22 11:54:50 +0300

    5-print

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - print if the number is positive, zero or negative
+*
+*Description: using the main function
+*this program prints "Programming is positive, zero or negative
+*Return: 0
+*/
+
+int main(void);
+{
+for (i = 0; i < 10; i++)[A[A[D[D[D[D[D[D[D[D[D[D[[B[B[C[C[C[C[C[C[C[C[C[C;
+{
+	printf("%i", i);
+}printf("\n");[D[D[D[D[D[D[D[D[D[D[D[D[D
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D
+printf[B[C[C[C[C[C[C[C[C[C[C[C[C
+return (0);
+}
